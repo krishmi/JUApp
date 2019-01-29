050d934e61519fe5542499ad3708f54db12c6027
krishna <krishmi139@gmail.com>
2019-01-29 22:55:42 +0530

    Completed different UI

--- JUApp/urls.py
+++ JUApp/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns=[
 	#path('test/',views.test,name='test'),
 	path('<placeholder>/',views.index,name='index'),
+	path('<placeholder>/create/',views.add,name='add'),
 	path('<placeholder>/verify/',views.verify,name='verify'),
 	path('<placeholder>/<dept>/',views.subject,name='subject'),
+	path('<placeholder>/<dept>/<subject>/',views.resource,name='resource'),
+	path('<placeholder>/<dept>/<subject>/<resource>/',views.uploading,name='upload'),
+	path('<placeholder>/<dept>/<subject>/<resource>/<res_name>',views.pdf_response,name='pdf'),
 #	path('<dept>/<subject>/',views.subject,name='subject'),
 #	path('<dept>/<subject>/<resource>/login/',views.login,name='login'),
 #	path('<dept>/<subject>/<resource>/login/create/',views.create,name='create'),
--- JUApp/views.py
+++ JUApp/views.py
@@ -6,7 +6,16 @@ from django.http import HttpResponse,HttpResponseRedirect
 import os
 
 # Create your views here.
+"""
 def index(request,placeholder):
+	if placeholder=='index':
+		return render(request,'index.html',{'flag':True})
+	else :
+		return render(request,'index.html',{'flag':False})
+"""
+def index(request,placeholder):
+	if request.session['flag']==True:
+		HttpResponseRedirect('/JUApp/'+request.session['uname']+'/')
 	if placeholder=='index':
 		return render(request,'index.html',{'flag':True})
 	else :
@@ -35,25 +44,28 @@ def subject(request,placeholder,dept):
 		s7=Subjects.objects.filter(dept=dept,sem='Seventh')
 		s8=Subjects.objects.filter(dept=dept,sem='Eighth')
 		return render(request,'index.html',{'flag':False,'s1':s1,'s2':s2,'s3':s3,'s4':s4,'s5':s5,'s6':s6,'s7':s7,'s8':s8})
-def subject(request,dept,subject):
+"""
+
+def resource(request,placeholder,dept,subject):
 	books=os.listdir('/home/krishna/JUApp/media/'+subject+'/'+'book/')
 	slides=os.listdir('/home/krishna/JUApp/media/'+subject+'/slide/')
 	notes=os.listdir('/home/krishna/JUApp/media/'+subject+'/note/')
 	questions=os.listdir('/home/krishna/JUApp/media/'+subject+'/question/')
-	return render(request,'subject.html',{'subject':subject,'books':books,'slides':slides,'notes':notes
-		,'questions':questions})
+	form =UploadForm()
+	return render(request,'resources.html',{'subject':subject,'books':books,'slides':slides,'notes':notes
+		,'questions':questions,'form':form})
 
-def pdf_response(request,subject,dept,resource,resource_name):
-	with open('/home/krishna/JUApp/media/'+subject+'/'+resource+'/'+resource_name, 'rb') as pdf:
+def pdf_response(request,placeholder,subject,dept,resource,res_name):
+	with open('/home/krishna/JUApp/media/'+subject+'/'+resource+'/'+res_name, 'rb') as pdf:
 		response = HttpResponse(pdf.read(), content_type='application/pdf')
 		response['Content-Disposition'] = 'inline'
 		return response
 	pdf.closed
-
+"""
 def login(request,dept,subject,resource):
 	return render(request,'login.html',{'flag':True,'sub':subject,'res':resource})
-
-def uploading(request,subject,dept,resource):
+"""
+def uploading(request,placeholder,subject,dept,resource):
 	inst=Upload()
 	inst.subject=subject
 	inst.resource=resource
@@ -61,10 +73,10 @@ def uploading(request,subject,dept,resource):
 	upload=UploadForm(request.POST,request.FILES,instance=inst)
 	if upload.is_valid():
 		upload.save(commit=True)
-		return HttpResponseRedirect('/JUApp/'+dept+'/'+subject+'/')
+		return HttpResponseRedirect('/JUApp/'+placeholder+'/'+dept+'/'+subject+'/')
 	else:
 		return HttpResponseRedirect('/JUApp/index')
-
+"""
 def create(request,dept,subject,resource):
 	return render(request,'signup.html')"""
 
@@ -98,14 +110,12 @@ def verify(request,placeholder):
 			return HttpResponseRedirect('/JUApp/index/')
 	else:
 		return HttpResponse('Not post method')
-"""
-def add(request,dept,subject,resource):
-	if request.method=="POST":
 
-		user=User(name=request.POST.get('name'),uname=request.POST.get('uname'),roll=request.POST.get('roll'),dept=request.POST.get('dept'),password=request.POST.get('psw'))
+def add(request,placeholder):
+	if request.method=="POST":
+		user=User(name=request.POST.get('name'),uname=request.POST.get('uname'),roll=request.POST.get('roll'),dept=request.POST.get('dept'),password=request.POST.get('passwd'))
 		user.save()
-		form=UploadForm()
-		return render(request,'login.html',{'flag':False,'sub':subject,'res':resource,'form':form})
+		return HttpResponseRedirect('/JUApp/'+request.POST.get('uname')+'/')
 	else:
-		return HttpResponse('Not post method')
-"""
+		return HttpResponse('Error!')
+
